6608bd421437c0b094d2e6d2303b1eaf6d5e6575
rajesh4210 <75352443+rajesh4210@users.noreply.github.com>
2021-10-20 19:48:57 +0530

    Import Export assign FoodApp2 & 3Added 19-10-21

---
+++ foodApp-II/components/header.js
@@ -0,0 +1,19 @@
+function navbar(){
+    return `<nav class="nav">
+    <div>
+    <h3><a href="/searchReciepe.html">Search Reciepes</a></h3>
+    <i class="material-icons">search</i>
+    </div>
+    <div>
+        <h3><a href="/recOfDay.html">Recipies of the day</a></h3>
+        <i class="material-icons">restaurant</i>
+    </div>
+    <div>
+    <h3><a href="/showLatRec.html">Latest Recipies</a></h3>
+    <i class="material-icons">lunch_dining</i>
+    </div>
+
+    </nav>`;
+}
+
+export default navbar;
