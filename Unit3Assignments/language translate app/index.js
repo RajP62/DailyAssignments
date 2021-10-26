759f607875f0375d99d2140a834d506232a62ae1
rajesh4210 <75352443+rajesh4210@users.noreply.github.com>
2021-10-27 01:08:18 +0530

    Language translate app 27-10-2021

---
+++ Unit3Assignments/language translate app/index.js
@@ -0,0 +1,63 @@
+// function translate(ev){
+//     ev.preventDefault();
+//     let inp_txt = document.getElementById('changeFrom').value;
+//     console.log(inp_txt)
+// }
+// var voices = require('responsivevoice';
+let sel_el = document.getElementById('translateTo');
+fetch('https://libretranslate.de/languages').then((res)=>{
+    return res.json();
+})
+.then((res)=>{
+    res.forEach(element => {
+        let option = document.createElement('option');
+        option.value = element.code;
+        option.textContent = element.name;
+        sel_el.append(option)
+    });
+})
+let form = document.getElementById("translateForm");
+form.addEventListener('onsubmit',showTxt);
+function showTxt(ev){
+    ev.preventDefault();
+    let inp_txt = document.getElementById('changeFrom').value;
+    try{
+        let data = fetch(`https://libretranslate.de/detect`,{
+            method:'POST',
+            body:JSON.stringify({q:inp_txt}),
+            headers:{
+            'Content-Type':'application/json',
+        }
+        });
+        data.then((res)=>{
+        return res.json();
+        })
+        .then((res)=>{
+        translate(res[0].language,inp_txt);
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+async function translate(lang,text){
+    let transTo = document.getElementById('translateTo').value;
+    try{
+        let body = await fetch(`https://libretranslate.de/translate`,{
+        method:'POST',
+        body:JSON.stringify({q:text,source:lang,target:transTo}),
+        headers:{
+            'Content-Type':'application/json',
+        }
+        })
+        let res = await body.json();
+        let displayIn = document.getElementById('changeTo');
+        displayIn.textContent = res.translatedText;
+        console.log(res.translatedText);
+        responsiveVoice.speak(res.translatedText);
+    }
+    catch(err){
+        console.log('err',err);
+    }
+}
