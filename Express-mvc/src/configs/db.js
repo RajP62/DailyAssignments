6e7a3eb88799e3fa40b211b0f1f432530f012cdd
Rajesh123 <rajesh4151999@gmail.com>
2021-12-11 00:50:21 +0530

    Express-mvc mod

--- Express-mvc/src/configs/db.js
+++ Express-mvc/src/configs/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
+require("dotenv").config();
 
 module.exports = async()=>{
-    return await mongoose.connect("mongodb+srv://raj4151999:raj4151999@cluster0.myaaa.mongodb.net/Express-mvc");
+    return await mongoose.connect(process.env.MONGODB_CLUSTOR);
 }
