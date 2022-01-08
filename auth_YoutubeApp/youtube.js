68c58a809e737c4c7bacdf7c69b80c31a7f5b13b
RajP62 <rajesh4151999@gmail.com>
2022-01-08 13:55:27 +0530

    changed youtube app

--- auth_YoutubeApp/youtube.js
+++ auth_YoutubeApp/youtube.js
@@ -2,19 +2,19 @@ let dataUser = JSON.parse(localStorage.getItem("dataUser"));
 let userName = dataUser[0].username;
 let spanEl = document.getElementById("displayName");
 spanEl.textContent = userName;
-
+let mainCont = document.getElementById("content");
+let key = "IMP-NOTE : PUT YOUR YOUTUBE_CLIENT_KEY HERE ONLY THEN IT WILL WORK";
 getTrendingData();
 
 async function getData(){
     let inp = document.getElementById("searchInp").value;
-    let body = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${inp}&type=video&key=AIzaSyAnBaW48ziKnGqp-wJcZoZuyWom_CNt55M&maxResults=20&part=snippet`);
+    let body = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${inp}&type=video&key=${key}&maxResults=20&part=snippet`);
     let res = await body.json();
     appendData(res.items);
 }
 
 async function getTrendingData(){
-    // GET https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&regionCode=IN&key=AIzaSyAnBaW48ziKnGqp-wJcZoZuyWom_CNt55M
-    let body = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&regionCode=IN&key=AIzaSyAnBaW48ziKnGqp-wJcZoZuyWom_CNt55M`);
+    let body = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&regionCode=IN&key=${key}`);
     let res = await body.json();
     appendTrendingData(res.items);
 }
@@ -35,7 +35,7 @@ function appendTrendingData(vedios){
 }
 
 
-let mainCont = document.getElementById("content");
+
 function appendData(vedios){
     mainCont.innerHTML = null;
     console.log(vedios)
@@ -58,31 +58,3 @@ function appendData(vedios){
 
 }
 
-/*
-etag: "d1do9qX2YVBi8i_L9CAJDzw3hyY"
-id: {kind: 'youtube#video', videoId: '-7_x8Unut1k'}
-kind: "youtube#searchResult"
-snippet:
-channelId: "UCIwFjwMjI0y7PDBVEO9-bkQ"
-channelTitle: "Justin Bieber"
-description: "Justice (Triple Chucks Deluxe) out now: https://JustinBieber.lnk.to/JusticeTCD Shop Justice merch: https://JustinBieber.lnk.to/OfficialShop​ Follow Justin: ..."
-liveBroadcastContent: "none"
-publishTime: "2021-03-26T05:26:13Z"
-publishedAt: "2021-03-26T05:26:13Z"
-thumbnails: {default: {…}, medium: {…}, high: {…}}
-title: "Justin Bieber - Name (Visualizer) ft. Tori Kelly"*/
-
-// Break
-
-// function appendData(vedio_data){
-      
-//     // mainCont.innerHTML = null;
-//     vedio_data.forEach(({id: { videoId }})=>{//why vedioId destructuring is not working-errorpoint1
-//         console.log(videoId)
-//         // let div = document.createElement("div");
-//         // div.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}/frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-//         // div.style.marginTop = "20px";
-//         // vedios.append(div);
-//     });
-
-// }
